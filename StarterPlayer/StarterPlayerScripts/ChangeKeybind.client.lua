55e922c284822aef7c6c51b71b38009505f3d37d
Emi <183315760+cahexis@users.noreply.github.com>
2025-12-04 11:23:57 -0600

    Update ChangeKeybind.client.lua

--- StarterPlayer/StarterPlayerScripts/ChangeKeybind.client.lua
+++ StarterPlayer/StarterPlayerScripts/ChangeKeybind.client.lua
@@ -4,7 +4,7 @@ local rep = game:GetService("ReplicatedStorage")
 local remote = rep.Windup
 local cooldown = false
 
-
+-- below is the code used for spawning the windup robot, having a cooldown to prevent spamming it.
 UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
 	if input.UserInputType == Enum.UserInputType.Keyboard then 
 		if input.KeyCode == Enum.KeyCode.Z then 
